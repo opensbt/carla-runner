efe41589b16ce5656e6300f7706f4e3d73b22861
Marc Pavel <pavel@in.tum.de>
2022-10-31 15:53:36 +0100

    Added the ros service calls (not tested)

--- controllers/fmi_agent.py
+++ controllers/fmi_agent.py
@@ -5,9 +5,10 @@
 # For a copy, see <https://opensource.org/licenses/MIT>.
 
 import carla
+import rospy
 
+from rosco.srv import *
 from visualizations.real import CameraView
-
 from srunner.autoagents.autonomous_agent import AutonomousAgent
 
 
@@ -16,6 +17,7 @@ class FMIAgent(AutonomousAgent):
     _agent = None
     _route_assigned = False
     _visual = None
+    _do_step_service = None
 
     def __init__(self, simulator):
         super().__init__("")
@@ -24,22 +26,37 @@ class FMIAgent(AutonomousAgent):
 
     def setup(self, _):
         self._agent = None
+        
+        #initialize and call the masterInit service
+        rospy.wait_for_service('master/init')
+        init_master = rospy.ServiceProxy('master/init', MasterInitService)
+        init_master('/opt/workspace/src/rosco/share')
+        
+        # initialize the doStepsUntil Service
+        rospy.wait_for_service('master/doStepsUntil')
+        _do_step_service = rospy.ServiceProxy('master/doStepsUntil', DoStepsUntilService, persistent=True)
 
     def run_step(self, input_data, _):
         if self._visual is not None:
             self._visual.run(input_data)
         
-        for point in input_data['lidar'][1]:
-            if point[0] < 3.0:
-                 return carla.VehicleControl(throttle=0.0,
-                                           steer=0.0,
-                                           brake=1.0,
-                                           hand_brake=False,
-                                           reverse=False,
-                                           manual_gear_shift=False,
-                                           gear=1)
+        print('Amount of data points: ' +  str(len(input_data['lidar'][1])))
         
-        return carla.VehicleControl(throttle=0.4,
+        #TODO manipulate all x values into one single distance value or publish each one (but this does too many service calls)
+        resp = _do_step_service(input_data['lidar'][1][0][1])
+        motorValue = resp.motorValue
+        
+        #TODO manipulate the motorValue into a reasonable VehicleControl object
+        if motorValue <= 0.3:
+            return carla.VehicleControl(throttle=0.0,
+                                        steer=0.0,
+                                        brake=1.0,
+                                        hand_brake=False,
+                                        reverse=False,
+                                        manual_gear_shift=False,
+                                        gear=1)
+        
+        return carla.VehicleControl(throttle=motorValue,
                                            steer=0.0,
                                            brake=0.0,
                                            hand_brake=False,
@@ -63,11 +80,11 @@ class FMIAgent(AutonomousAgent):
         sensors.append(
                 {
                     'type': 'sensor.lidar.ray_cast',
-                    'x': 1.6, 'y': 0.0, 'z': 1.60,
+                    'x': 1.6, 'y': 0.0, 'z': 0.5,
                     'roll': 0.0, 'pitch': 0.0, 'yaw': 180.0,
-                    'channels': 1, 'range': 10.0, 'points_per_second': 1000,
-                    'rotation_frequency': 0.0, 'upper_fov': 0.1, 'lower_fov': 0.0,
-                    'horizontal_fov': 0.1, 'id': 'lidar'
+                    'channels': 8, 'range': 10.0, 'points_per_second': 10000,
+                    'rotation_frequency': 0.0, 'upper_fov': 10.0, 'lower_fov': 0.0,
+                    'horizontal_fov': 90.0, 'id': 'lidar'
                 }
             )
         return sensors

758f333d770954e9252fc47c38daf392e090d439
Marc Pavel <pavel@in.tum.de>
2022-11-13 14:15:48 +0100

    Added sensors /send multiple data via service call

--- controllers/fmi_agent.py
+++ controllers/fmi_agent.py
@@ -6,6 +6,7 @@
 
 import carla
 import rospy
+from numpy.linalg import norm
 
 from rosco.srv import *
 from visualizations.real import CameraView
@@ -47,16 +48,41 @@ class FMIAgent(AutonomousAgent):
             self._visual.run(input_data)
         
         print('Amount of data points: ' + str(len(input_data['lidar'][1])))
+        print('Amount of left lidar data points: ' + str(len(input_data['lidar_left'][1])))
+        print('Amount of right lidar data points: ' + str(len(input_data['lidar_right'][1])))
+
+        sensor_data = []
+        names = []
               
         #TODO manipulate all x values into one single distance value or publish each one (but this does too many service calls)
+        #distance_to_front
+        names.append("distance_to_front")
         if(len(input_data['lidar'][1]) > 0):
-            print('calling do step service with value: ', input_data['lidar'][1][0][0])
-            resp = self._do_step_service(input_data['lidar'][1][0][0], rospy.get_rostime())
+            sensor_data.append(norm(input_data['lidar'][1][0][:-1]))
+            print('calling do step service with value: ', norm(input_data['lidar'][1][0][:-1]))
         else:
-            resp = self._do_step_service(15.0, rospy.get_rostime())
+            sensor_data.append(15.0)
             print('calling do step service with value: ', 15.0)
+          
+        #distance_to_front_left
+        names.append("distance_to_front_left")
+        if(len(input_data['lidar_left'][1]) > 0):
+            sensor_data.append(norm(input_data['lidar_left'][1][0][:-1]))
+            print('calling do step service with LEFT value: ', norm(input_data['lidar_left'][1][0][:-1]))
+        else:
+            sensor_data.append(15.0)
+            print('calling do step service with LEFT value: ', 15.0)
+           
+        #distance_to_front_right
+        names.append("distance_to_front_right")
+        if(len(input_data['lidar_right'][1]) > 0):
+            sensor_data.append(norm(input_data['lidar_right'][1][0][:-1]))
+            print('calling do step service with RIGHT value: ', norm(input_data['lidar_right'][1][0][:-1]))
+        else:
+            sensor_data.append(15.0)
+            print('calling do step service with RIGHT value: ', 15.0)
 
-
+        resp = self._do_step_service(sensor_data, names, rospy.get_rostime())
         motorValue = resp.motorValue
         print("Response of do step service is: ", motorValue)
         
@@ -96,9 +122,29 @@ class FMIAgent(AutonomousAgent):
                     'type': 'sensor.lidar.ray_cast',
                     'x': 1.6, 'y': 0.0, 'z': 0.5,
                     'roll': 0.0, 'pitch': 0.0, 'yaw': 180.0,
-                    'channels': 8, 'range': 10.0, 'points_per_second': 10000,
+                    'channels': 1, 'range': 10.0, 'points_per_second': 100,
                     'rotation_frequency': 0.0, 'upper_fov': 10.0, 'lower_fov': 0.0,
                     'horizontal_fov': 90.0, 'id': 'lidar'
+                }
+            )
+        sensors.append(
+                {
+                    'type': 'sensor.lidar.ray_cast',
+                    'x': 1.6, 'y': -0.5, 'z': 1.60,
+                    'roll': 0.0, 'pitch': 0.0, 'yaw': 135.0,
+                    'channels': 8, 'range': 10.0, 'points_per_second': 100,
+                    'rotation_frequency': 0.0, 'upper_fov': 5.0, 'lower_fov': -5.0,
+                    'horizontal_fov': 30, 'id': 'lidar_left'
+                }
+            )
+        sensors.append(
+                {
+                    'type': 'sensor.lidar.ray_cast',
+                    'x': 1.6, 'y': 0.5, 'z': 1.60,
+                    'roll': 0.0, 'pitch': 0.0, 'yaw': 225.0,
+                    'channels': 8, 'range': 10.0, 'points_per_second': 100,
+                    'rotation_frequency': 0.0, 'upper_fov': 5.0, 'lower_fov': -5.0,
+                    'horizontal_fov': 30, 'id': 'lidar_right'
                 }
             )
         return sensors

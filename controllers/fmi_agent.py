6932ce40c483a1d983ae417b3428c171970d296b
Marc Pavel <pavel@in.tum.de>
2022-12-03 15:59:21 +0100

    works with adapted rover model

--- controllers/fmi_agent.py
+++ controllers/fmi_agent.py
@@ -22,7 +22,7 @@ class FMIAgent(AutonomousAgent):
     _visual = None
     _do_step_service = None
     _speed = None
-    _previous_speed = 0.5
+    _previous_speed = [0.5, 0.5, 0.5]
     _ego = None
 
     def __init__(self, simulator):
@@ -36,10 +36,7 @@ class FMIAgent(AutonomousAgent):
         for actor in CarlaDataProvider.get_world().get_actors():
             if 'role_name' in actor.attributes and actor.attributes['role_name'] == 'hero':
                 self._ego = actor
-                break
-        print(type(self._ego))
-        
-        print('----   Setup function    ----')
+                break        
         
         rospy.init_node('fmi_agent')
         
@@ -58,40 +55,43 @@ class FMIAgent(AutonomousAgent):
         if self._visual is not None:
             self._visual.run(input_data)
         
-        #TODO
-        #print('Amount of data points: ' + str(len(input_data['lidar'][1])))
-        #print('Amount of left lidar data points: ' + str(len(input_data['lidar_left'][1])))
-        #print('Amount of right lidar data points: ' + str(len(input_data['lidar_right'][1])))
-        
         signals = SignalsMessage()
               
-        #TODO manipulate all x values into one single distance value or publish each one (but this does too many service calls)
         #distance_to_front
-        if(len(input_data['lidar'][1]) > 0):
-            signals.floatSignals.append(FloatSignal("DistanceToFrontLaser", norm(input_data['lidar'][1][0][:-1])))
-            print('calling do step service with value: ', norm(input_data['lidar'][1][0][:-1]))
-        else:
-            signals.floatSignals.append(FloatSignal("DistanceToFrontLaser", 15.0))
-            print('calling do step service with value: ', 15.0)
+        minDistance = 2.0
+        for point in input_data['lidar'][1]:
+            tmp = norm(point[:-1])/10.0
+            if (tmp < minDistance):
+                minDistance = tmp
+        
+        signals.floatSignals.append(FloatSignal("DistanceToFrontLaser", minDistance))
+        print('calling do step service with value: ', minDistance)
+        
           
         #distance_to_front_left
-        if(len(input_data['lidar_left'][1]) > 0):
-            signals.floatSignals.append(FloatSignal("DistanceToFrontUS_left", norm(input_data['lidar_left'][1][0][:-1])))
-            print('calling do step service with LEFT value: ', norm(input_data['lidar_left'][1][0][:-1]))
-        else:
-            signals.floatSignals.append(FloatSignal("DistanceToFrontUS_left", 15.0))
-            print('calling do step service with LEFT value: ', 15.0)
+        minDistance = 2.0
+        for point in input_data['lidar_left'][1]:
+            tmp = norm(point[:-1])/10.0
+            if (tmp < minDistance):
+                minDistance = tmp
+        
+        signals.floatSignals.append(FloatSignal("DistanceToFrontUS_left", minDistance))
+        print('calling do step service with LEFT value: ', minDistance)
+        
            
         #distance_to_front_right
-        if(len(input_data['lidar_right'][1]) > 0):
-            signals.floatSignals.append(FloatSignal("DistanceToFrontUS_right", norm(input_data['lidar_right'][1][0][:-1])))
-            print('calling do step service with RIGHT value: ', norm(input_data['lidar_right'][1][0][:-1]))
-        else:
-            signals.floatSignals.append(FloatSignal("DistanceToFrontUS_right", 15.0))
-            print('calling do step service with RIGHT value: ', 15.0)
-            
-        signals.floatSignals.append(FloatSignal("VelocityIn", CarlaDataProvider.get_velocity(self._ego)))
-        print('calling do step service with velocity in value: ', CarlaDataProvider.get_velocity(self._ego))
+        minDistance = 2.0
+        for point in input_data['lidar_right'][1]:
+            tmp = norm(point[:-1])/10.0
+            if (tmp < minDistance):
+                minDistance = tmp
+        
+        signals.floatSignals.append(FloatSignal("DistanceToFrontUS_right", minDistance))
+        print('calling do step service with RIGHT value: ', minDistance)
+        
+        #VelocityIn
+        signals.floatSignals.append(FloatSignal("VelocityIn", 0.4 if self._previous_speed[0] > 0.4 else self._previous_speed[0]))
+        print('calling do step service with velocity in value: ', 0.4 if self._previous_speed[0] > 0.4 else self._previous_speed[0])
 
         resp = self._do_step_service(signals, rospy.get_rostime())
         
@@ -100,9 +100,11 @@ class FMIAgent(AutonomousAgent):
                 self._speed = float_signal.value
                 print("Response for ", float_signal.name, " is: ", self._speed)
                 
-        #TODO manipulate the motorValue into a reasonable VehicleControl object
-        if self._speed <= 0.2 and self._previous_speed > self._speed:
-            self._previous_speed = self._speed
+        if self._speed <= 0.2 and (self._previous_speed[0] > self._speed or 
+                                   self._previous_speed[1] > self._speed or self._previous_speed[2] > self._speed):
+            print('full brake')
+            self._previous_speed.pop()
+            self._previous_speed.insert(0, self._speed)
             return carla.VehicleControl(throttle=0.0,
                                         steer=0.0,
                                         brake=1.0,
@@ -110,21 +112,24 @@ class FMIAgent(AutonomousAgent):
                                         reverse=False,
                                         manual_gear_shift=False,
                                         gear=1)
-        elif self._previous_speed > self._speed:
-            self._previous_speed = self._speed
+        elif self._previous_speed[0] > self._speed or self._previous_speed[1] > self._speed or self._previous_speed[2] > self._speed:
+            print('brake')
+            self._previous_speed.pop()
+            self._previous_speed.insert(0, self._speed)
             return carla.VehicleControl(throttle=0.0,
                                         steer=0.0,
-                                        brake=0.1,
+                                        brake=0.5,
                                         hand_brake=False,
                                         reverse=False,
                                         manual_gear_shift=False,
                                         gear=1)
         
-        if self._speed > 0.5:
-            self._speed = 0.5
+        self._previous_speed.pop()
+        self._previous_speed.insert(0, self._speed)
+
+        if self._speed > 0.4:
+            self._speed = 0.4
             
-        self._previous_speed = self._speed
-        
         return carla.VehicleControl(throttle=self._speed,
                                            steer=0.0,
                                            brake=0.0,
@@ -151,7 +156,7 @@ class FMIAgent(AutonomousAgent):
                     'type': 'sensor.lidar.ray_cast',
                     'x': 1.6, 'y': 0.0, 'z': 0.5,
                     'roll': 0.0, 'pitch': 0.0, 'yaw': 180.0,
-                    'channels': 1, 'range': 10.0, 'points_per_second': 100,
+                    'channels': 1, 'range': 20.0, 'points_per_second': 100,
                     'rotation_frequency': 0.0, 'upper_fov': 10.0, 'lower_fov': 0.0,
                     'horizontal_fov': 90.0, 'id': 'lidar'
                 }

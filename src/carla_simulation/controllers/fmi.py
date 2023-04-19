4a9cbaffc7f1eb3255695291c1f29507b84a891e
Eddie Groh <groh@fortiss.org>
2023-04-19 10:31:12 +0200

    Corrected steering input signals to work properly with the rover model

--- src/carla_simulation/controllers/fmi.py
+++ src/carla_simulation/controllers/fmi.py
@@ -100,24 +100,41 @@ class FMIAgent(AutonomousAgent):
         distance_left = distance_lane_from_middle + signed_distance
         distance_right = distance_lane_from_middle - signed_distance
 
-        print(f"Lane distances: left: {distance_left} right: {distance_right}")
+        # print(f"Lane distances: left: {distance_left} right: {distance_right}")
 
+        # Add signals for correct lane detection function
         signals.floatSignals.append(FloatSignal("LD_Distance_Right", distance_right))
         signals.floatSignals.append(FloatSignal("LD_Distance_Left", distance_left))
 
+        signals.boolSignals.append(BoolSignal("LD_server_connected", True))
+        signals.boolSignals.append(BoolSignal("LD_present_right", True))
+        signals.boolSignals.append(BoolSignal("LD_present_left", True))
 
         resp = self._do_step_service(signals, rospy.get_rostime())
 
+        # Search for steering and motor value in the signals
+        steering = None
         for float_signal in resp.result.floatSignals:
             if float_signal.name == 'MotorValue':
                 self._speed = float_signal.value
+            elif float_signal.name == "SteeringValue":
+                steering = float_signal.value
+
+        if not steering:
+            print("SteeringValue not found. Assuming no steering")
+            steering = 6000.0
+
+        # Converting steering to -1.0 to 1.0
+        # 6000 is middle, and maxiumum deviation is 1800 to either side
+        steering -= 6000.0
+        steering /= 1800.0
 
         if self._speed <= 0.2 and (self._previous_speed[0] > self._speed or
                                    self._previous_speed[1] > self._speed or self._previous_speed[2] > self._speed):
             self._previous_speed.pop()
             self._previous_speed.insert(0, self._speed)
             return carla.VehicleControl(throttle=0.0,
-                                        steer=0.0,
+                                        steer=steering,
                                         brake=1.0,
                                         hand_brake=False,
                                         reverse=False,
@@ -127,7 +144,7 @@ class FMIAgent(AutonomousAgent):
             self._previous_speed.pop()
             self._previous_speed.insert(0, self._speed)
             return carla.VehicleControl(throttle=0.0,
-                                        steer=0.0,
+                                        steer=steering,
                                         brake=0.5,
                                         hand_brake=False,
                                         reverse=False,
@@ -141,7 +158,7 @@ class FMIAgent(AutonomousAgent):
             self._speed = 0.4
 
         return carla.VehicleControl(throttle=self._speed,
-                                           steer=0.0,
+                                           steer=steering,
                                            brake=0.0,
                                            hand_brake=False,
                                            reverse=False,

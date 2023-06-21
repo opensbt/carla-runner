1a3f3445dc9dc5858feb012b4401a4fb14b09f60
Marc <pavel@fortiss.org>
2023-06-21 16:07:17 +0200

    WIP: Added function to spawn props

--- src/carla_simulation/utils/utility.py
+++ src/carla_simulation/utils/utility.py
@@ -17,11 +17,11 @@ def untoggle_environment_objects(world, semantic_tags):
         env_obj_ids = [obj.id for obj in env_obj]
         world.enable_environment_objects(set(env_obj_ids), True)
 
-def change_color_texture_of_objects(world, filter_criteria, color, width, height):
+def change_color_texture_of_objects(world, filter_criteria, color, width, height, material):
     objs = world.get_names_of_all_objects()
     filtered_objs = list(filter(lambda k: filter_criteria in k, objs))
 
-    world.apply_color_texture_to_object(filtered_objs, carla.MaterialParameter.Diffuse, 
+    world.apply_color_texture_to_object(filtered_objs, material, 
                                     get_texture(color, width, height))
 
 def get_texture(color, width, height):
@@ -30,3 +30,17 @@ def get_texture(color, width, height):
             for y in range(0, height):
                 texture.set(x, y, color)
         return texture
+
+def spawn_props(world, prop):
+    bp = world.get_blueprint_library().find(prop)
+
+    world.spawn_actor(bp, carla.Transform(carla.Location(-25.5, 120.0, 0.0 ), carla.Rotation(0, 0, 0)))
+    world.spawn_actor(bp, carla.Transform(carla.Location(-28.0, 124.0, 0.0 ), carla.Rotation(0, 0, 0)))
+    world.spawn_actor(bp, carla.Transform(carla.Location(-31.0, 128.0, 0.0 ), carla.Rotation(0, 0, 0)))
+    world.spawn_actor(bp, carla.Transform(carla.Location(-34.5, 131.0, 0.0 ), carla.Rotation(0, 0, 0)))
+    world.spawn_actor(bp, carla.Transform(carla.Location(-38.5, 134.0, 0.0 ), carla.Rotation(0, 0, 0)))
+    world.spawn_actor(bp, carla.Transform(carla.Location(-20.5, 120.0, 0.0 ), carla.Rotation(0, 0, 0)))
+    world.spawn_actor(bp, carla.Transform(carla.Location(-23.0, 124.0, 0.0 ), carla.Rotation(0, 0, 0)))
+    world.spawn_actor(bp, carla.Transform(carla.Location(-26.5, 128.0, 0.0 ), carla.Rotation(0, 0, 0)))
+    world.spawn_actor(bp, carla.Transform(carla.Location(-30.0, 132.0, 0.0 ), carla.Rotation(0, 0, 0)))
+    world.spawn_actor(bp, carla.Transform(carla.Location(-34.5, 136.0, 0.0 ), carla.Rotation(0, 0, 0)))

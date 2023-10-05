c558736e51deba951c2046e6326b3237920ee269
Sebastian Bergemann <bergemann@fortiss.org>
2023-10-05 14:15:01 +0000

    Update test.py to be consistent with the new balancer parameters

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ from carla_simulation.balancer import Balancer
 # Please use "git update-index --assume-unchanged test.py" if you use and change this file to disable pushing your local changes for it on the server!
 
 balancer = Balancer(
-    # If the path changes, the container needs to be rebuilt.
+    # If the path changes or the quality, the container needs to be rebuilt.
     # Delete all client containers for this to happen (docker container rm <client-container-name>, e.g., carla-client-0).
     directory='/path/to/your/test/scenarios',
     agent='FMIAgent',
@@ -15,7 +15,8 @@ balancer = Balancer(
     keep_carla_servers=False,
     resolution = 0.1,
     synchronous = True,
-    enable_manual_control = False
+    enable_manual_control = False,
+    quality = "Low" # Low, Medium, Epic
 )
 
 try:
